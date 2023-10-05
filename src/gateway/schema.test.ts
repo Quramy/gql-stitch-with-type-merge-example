5e06ac8bd62dccf96bd1f5191e7674cce0df2f03
Quramy <yosuke.kurami@gmail.com>
2023-10-05 16:09:18 +0900

    modify test codes

--- src/gateway/schema.test.ts
+++ src/gateway/schema.test.ts
@@ -19,16 +19,10 @@ const postQueryMock = jest
 jest.mock('./subSchemas', () => {
   return {
     loadSubSchemas: async () => {
-      const [userTypeDefs] = await loadFiles(
-        'src/user-service/typeDefs.graphql',
-      )
-      const userServiceSchema = makeExecutableSchema({
-        typeDefs: print(userTypeDefs),
-      })
+      const userTypeDefs = await loadFiles('src/user-service/typeDefs.graphql')
+      const userServiceSchema = makeExecutableSchema({ typeDefs: userTypeDefs })
 
-      const [postTypeDefs] = await loadFiles(
-        'src/post-service/typeDefs.graphql',
-      )
+      const postTypeDefs = await loadFiles('src/post-service/typeDefs.graphql')
       const postServiceSchema = makeExecutableSchema({ typeDefs: postTypeDefs })
 
       return {
@@ -55,8 +49,8 @@ describe('gateway schema', () => {
     postQueryMock.mockClear()
   })
 
-  describe('stitching', () => {
-    test('userService then postService', async () => {
+  describe('leraning stitching with type merge', () => {
+    test('merge postService result into userService result', async () => {
       userQueryMock.mockReturnValue({
         user: () => ({ id: 'test_user' }),
       })
@@ -72,7 +66,7 @@ describe('gateway schema', () => {
         }
 
         query {
-          user(id: "user") {
+          user(id: "test_user") {
             id
             ...FieldsFromPostService
           }
@@ -82,18 +76,19 @@ describe('gateway schema', () => {
       expect(data).toMatchObject({ user: { posts: [{ id: 'test_post' }] } })
     })
 
-    test('postService then userService', async () => {
-      userQueryMock.mockReturnValue({
-        users: () => [
-          { id: 'test_user1', name: 'user 1' },
-          { id: 'test_user2', name: 'user 2' },
-        ],
-      })
+    test('merge userService result into postService result', async () => {
       postQueryMock.mockReturnValue({
         popularPosts: () => [
           { id: 'test_post1', author: { id: 'test_user1' } },
           { id: 'test_post2', author: { id: 'test_user2' } },
           { id: 'test_post3', author: { id: 'test_user1' } },
+        ],
+      })
+
+      userQueryMock.mockReturnValue({
+        users: () => [
+          { id: 'test_user1', name: 'user 1' },
+          { id: 'test_user2', name: 'user 2' },
         ],
       })
 
